889720104bc20cb82476de55d743fdf696d51f39
Mukhwinder Singh <w0828714@myscc.ca>
2024-09-25 16:15:13 -0400

    I completed till step 4 and created two components.

---
+++ src/app/smartphone-list-item/smartphone-list-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-smartphone-list-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './smartphone-list-item.component.html',
+  styleUrl: './smartphone-list-item.component.css'
+})
+export class SmartphoneListItemComponent {
+
+}
---
+++ src/app/smartphone-list/smartphone-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-smartphone-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './smartphone-list.component.html',
+  styleUrl: './smartphone-list.component.css'
+})
+export class SmartphoneListComponent {
+
+}
