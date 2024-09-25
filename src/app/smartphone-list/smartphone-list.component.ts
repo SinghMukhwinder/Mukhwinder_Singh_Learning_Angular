2f0b15513f3706708b650b91ac7d2dce835e1503
Mukhwinder Singh <w0828714@myscc.ca>
2024-09-25 16:21:54 -0400

    I completed till step 4 and created two components and added NgForOf to import

--- src/app/smartphone-list/smartphone-list.component.ts
+++ src/app/smartphone-list/smartphone-list.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-smartphone-list',
   standalone: true,
-  imports: [],
+  imports: [NgForOf],
   templateUrl: './smartphone-list.component.html',
   styleUrl: './smartphone-list.component.css'
 })
 export class SmartphoneListComponent {
-
 }
