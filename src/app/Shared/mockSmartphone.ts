eaa84a4271dc83eb8cd3492886328740be6a7e56
Mukhwinder Singh <w0828714@myscc.ca>
2024-10-03 14:09:16 -0400

    I have done my 4 and 5 step.

---
+++ src/app/Shared/mockSmartphone.ts
@@ -0,0 +1,11 @@
+import {Smartphone} from "./Models/Smartphones";
+
+export  const smartphones: Smartphone [] = [
+   {serialNumber: "1234A", brand: "Apple", model: "IPhone13", storage:  256, isAvailable: true},
+ {serialNumber: "1234B", brand: "Samsung", model: "S23", storage:  512, isAvailable: true},
+ {serialNumber: "1234C", brand: "One Plus", model: "5T", storage:  128, isAvailable: true},
+ {serialNumber: "1234D", brand: "Vivo", model: "V15", storage:  256, isAvailable: false},
+ {serialNumber: "1234E", brand: "RealMe", model: "GT neo 3T", storage:  64, isAvailable: false},
+{serialNumber: "1234E", brand: "RealMe", model: "GT neo 3T", storage:  64, isAvailable: false}
+
+];
