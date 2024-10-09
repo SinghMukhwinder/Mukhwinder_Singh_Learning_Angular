46e6b3bfc556b37d4e3bb63fdce3db5ff813e499
Mukhwinder Singh <w0828714@myscc.ca>
2024-10-09 14:06:50 -0400

    Set up the router and Defined the list of routes.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,19 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {provideRouter, Routes} from "@angular/router";
+import {SmartphoneListComponent} from "./app/smartphone-list/smartphone-list.component";
+import {SmartphoneListItemComponent} from "./app/smartphone-list-item/smartphone-list-item.component";
+
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+const routes: Routes = [
+  {path: 'smartphoneList', component: SmartphoneListComponent },
+  {path: 'smartphoneList/:serialNumber', component: SmartphoneListItemComponent }
+];
+
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes)]
+});
