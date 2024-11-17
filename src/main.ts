6e4367a2416924015c79079c783c889ad041ef16
Mukhwinder Singh <w0828714@myscc.ca>
2024-11-17 01:13:05 -0500

    Implemented lazy loading.

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,6 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import {provideRouter, Routes} from "@angular/router";
-import {SmartphoneListComponent} from "./app/smartphone-list/smartphone-list.component";
-import {SmartphoneListItemComponent} from "./app/smartphone-list-item/smartphone-list-item.component";
-import {ModifyListItemComponent} from "./app/modify-list-item/modify-list-item.component";
-import {PageNotFoundComponent} from "./app/page-not-found/page-not-found.component";
 import {provideHttpClient} from "@angular/common/http";
 import {importProvidersFrom} from "@angular/core";
 import {HttpClientInMemoryWebApiModule} from "angular-in-memory-web-api";
@@ -16,10 +12,10 @@ import {InMemoryDataService} from "./app/Services/in-memory-data.service";
 
 const routes: Routes = [
   {path: '', redirectTo: '/smartphones', pathMatch: 'full'}, // default route
-  {path: 'smartphones', component: SmartphoneListComponent },
-  {path: 'smartphones/:id', component: SmartphoneListItemComponent },
-  {path: 'modify-list-item', component: ModifyListItemComponent },
-  {path: '**', component: PageNotFoundComponent } // WildCard route for 404 page
+  {path: 'smartphones', loadComponent:() => import('./app/smartphone-list/smartphone-list.component').then(m => m.SmartphoneListComponent)},
+  {path: 'smartphones/:id', loadComponent:()=> import('./app/smartphone-list-item/smartphone-list-item.component').then(m => m.SmartphoneListItemComponent) },
+  {path: 'modify-list-item', loadComponent: () => import('./app/modify-list-item/modify-list-item.component').then(m => m.ModifyListItemComponent) },
+  {path: '**', loadComponent:() => import('./app/page-not-found/page-not-found.component').then(m => m.PageNotFoundComponent) } // WildCard route for 404 page
 ];
 
 
