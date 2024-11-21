37b3a434c8a1d1122a9c8ccaf1dd3d292f605216
Mukhwinder Singh <w0828714@myscc.ca>
2024-11-21 13:52:19 -0500

    Made HoverHighlight Directive and applied it on buttons.

--- src/app/smartphone-list/smartphone-list.component.ts
+++ src/app/smartphone-list/smartphone-list.component.ts
@@ -6,13 +6,14 @@ import {SmartphoneService} from "../Services/smartphone.service";
 import {Router, RouterLink} from "@angular/router";
 import {smartphones} from "../Shared/mockSmartphone";
 import {SmartphoneDetailsPipe} from "../pipes/smartphone-details.pipe";
+import {HoverHighlightDirective} from "../directives/hover-highlight.directive";
 
 
 
 @Component({
   selector: 'app-smartphone-list',
   standalone: true,
-  imports: [NgForOf, SmartphoneListItemComponent, NgClass, NgOptimizedImage, RouterLink, NgIf, UpperCasePipe, CurrencyPipe, DatePipe, SmartphoneDetailsPipe],
+  imports: [NgForOf, SmartphoneListItemComponent, NgClass, NgOptimizedImage, RouterLink, NgIf, UpperCasePipe, CurrencyPipe, DatePipe, SmartphoneDetailsPipe, HoverHighlightDirective],
   templateUrl: './smartphone-list.component.html',
   styleUrl: './smartphone-list.component.css'
 })
